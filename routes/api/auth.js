25f7b9ebea68f9ca85dd552fb96247e75e1010dd
Denys Kochergin <dangerspace7@gmail.com>
2022-09-17 14:15:19 +0300

    added an avatar during registration, as well as the ability to save and update the avatar by the user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const controllers = require("../../controllers/auth");
 const { schemas } = require("../../models/users");
 const { controllersWrapper } = require("../../helpers");
-const { validationBody,authenticate } = require("../../middlewares")
+const { validationBody,authenticate,upload } = require("../../middlewares")
 
 const router = express.Router();
 
@@ -27,6 +27,13 @@ router.patch(
   authenticate,
   validationBody(schemas.subscriptionSchema),
   controllersWrapper(controllers.updateSubscription)
+);
+
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  controllersWrapper(controllers.updateAvatar)
 );
 
 module.exports = router;
